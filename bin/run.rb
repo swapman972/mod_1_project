1807b699941587acaca89ef78be4d5114c59542f
swapman972 <jordan.melidor@hotmail.fr>
2020-04-14 15:53:28 -0400

    run file modified

--- bin/run.rb
+++ bin/run.rb
@@ -2,17 +2,32 @@ require 'rest-client'
 require_relative '../config/environment'
 require 'pry'
 
+# In this def we will ask the user for his name and add him to the trainer table
 def welcome_user
     puts "Hello new trainer, what is your name?"
     user_name = gets.chomp
+    Trainer.create(name: user_name)
     user_name
 end
 
+# In this def we are asking the user to pick a starter pokemon (which has to be one of the three to advance in the app)
+# then adding that pokemon to the pokemon
 def pick_starter
-    puts "What is your fav food"
-    fav_food = gets.chomp
-    food = Food.find_or_create_by(name: fav_food)
-    food
+    puts "Pick a starter pokemon between bulbasaur, charmander and squirtle"
+    pokemon = gets.chomp
+    while pokemon != "bulbasaur" || pokemon != "charmander" || pokemon != "squirtle" do
+        puts "Please pick a starter pokemon between bulbasaur, charmander and squirtle"
+        pokemon = gets.chomp
+    end
+
+    if pokemon == "bulbasaur"
+        Pokemon.create(species: pokemon, type: "grass")
+    elsif pokemon == "charmander"
+        Pokemon.create(species: pokemon, type: "fire")
+    elsif pokemon == "squirtle"
+        Pokemon.create(species: pokemon, type: "water")
+    end
+    pokemon
 end
 
 def run
@@ -20,7 +35,6 @@ def run
     puts "Welcome to the Elite 3 #{user_name}!!"
     food = get_food
     puts "Awesome! #{food.name} is my fav too"
-
 end
 
 run
--- db/migrate/20200414183647_create_pokemon.rb
+++ db/migrate/20200414183647_create_pokemon.rb
@@ -1,7 +1,7 @@
 class CreatePokemon < ActiveRecord::Migration[6.0]
   def change
     create_table :pokemons do |t|
-      t.string :nickname
+      #t.string :nickname
       t.string :species
       t.string :type
     end
